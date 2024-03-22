ef571ea63d7dc3c827450258b1f64646bc60cab8
Phil <philippe.teigne@gmail.com>
2024-03-21 23:22:12 -0400

    add in method tests

--- main.rb
+++ main.rb
@@ -5,5 +5,20 @@ require './candidates'
 require './filters'
 
 ## Your test code can go here
+puts "checks if candidate is experienced"
+puts experienced?(@candidates[0])
+puts "-----"
+
+puts "find should return candidate with matching id"
+puts find(5)
+puts "-----"
+
+puts "filter candidates to only include qualified candidates"
+puts qualified_candidates(@candidates)
+puts "-----"
+
+puts "sort candidates by most experience then github points"
+puts ordered_by_qualifications(@candidates)
+puts "-----"
 
 pp @candidates
